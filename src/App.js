b82f9156805f0caf8331f6915165a600e9faf02b
pik-r nawasena <153299031+adiwebpro@users.noreply.github.com>
2025-07-10 14:39:28 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from './pages/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
-import Blog from './pages/Blog';
+
 import Contact from './pages/Contact';
 import Footer from './pages/Footer';
 
@@ -20,7 +20,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/blog" element={<Blog />} />
+            
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
